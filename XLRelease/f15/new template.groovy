001ff9b6e4beb42dcd905d805a1a26fc3625b457
Release Administrator <>
2021-03-11 12:55:37 +0100

    fsafdsaf

---
+++ XLRelease/f15/new template.groovy
@@ -0,0 +1,20 @@
+// Exported from:        http://Jans-MBP.home:5516/#/templates/Folder9fea865f2f0c437db7921085cfbdb752-Release86c423e53cc3441da74a93c5c1233e13/code
+// Release version:      10.0.0-20210203-123974
+// Date created:         Thu Mar 11 12:55:37 CET 2021
+
+xlr {
+  template('new template') {
+    folder('f15')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2021-03-11T09:00:00+0100')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('a') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
